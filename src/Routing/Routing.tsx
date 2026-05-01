c584adce4b15d46874f8c353e8f7431350e55d07
Arjun4592-art <arjunmishra769@gmail.com>
2026-05-01 13:39:40 +0530

    Complete routing setup

--- src/Routing/Routing.tsx
+++ src/Routing/Routing.tsx
@@ -1,9 +1,20 @@
 import { Route, Routes } from 'react-router'
+import Home from '../components/Home'
+import Courses from '../components/Courses'
+import About from '../components/About'
+import Mentors from '../components/Mentors'
+import Success from '../components/Success'
+import Contact from '../components/Contact'
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/' element={<Home />} />
+      <Route path='/courses' element={<Courses />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/mentors' element={<Mentors />} />
+      <Route path='/success' element={<Success />} />
+      <Route path='/contact' element={<Contact />} />
     </Routes>
   )
 }
